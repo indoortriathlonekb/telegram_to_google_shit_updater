c6e0b94dd052044ea07af198107aa3b3fd13a4c5
indoortriathlonekb <indoortriathlonekb@gmail.com>
2025-10-22 17:09:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ import express from "express";
 import axios from "axios";
 import TelegramBot from "node-telegram-bot-api";
 
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
 const app = express();
 app.use(express.json());
 
